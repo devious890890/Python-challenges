495b8475baa2cff5e1d2d3ce62c035214211f484
devious890890 <vmuigaim@gmail.com>
2026-04-18 21:24:35 +0300

    commit3

---
+++ mean operator.py
@@ -0,0 +1,12 @@
+mean1 = 38
+wrong_number = 36
+correct_number= 56
+total_numbers= 40
+
+sum = mean1*total_numbers
+
+sum2 = sum-((wrong_number) - (correct_number))
+print("sum-((wrong_number) - correct_number) is equals", sum2)
+
+mean2 = sum2/total_numbers
+print("The new mean is",mean2)
