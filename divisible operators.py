28c7ffe48af648e33cf5c7c4362dbaa92a6783ad
devious890890 <vmuigaim@gmail.com>
2026-04-18 21:13:39 +0300

    commit1

---
+++ divisible operators.py
@@ -0,0 +1,9 @@
+print("Enter Numerator here: ")
+x = int(input())
+print("Enter Denominator here: ")
+y = int(input())
+
+if x%y==0:
+    print("\n" +str (x)+ "is divible by"+ str (y))
+else:
+    print("\n"  +str (x)+ "is not divisible bye"+ str (y))
