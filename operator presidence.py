4d6d78fbcf7326184f59246da1b703a2aef614ed
devious890890 <vmuigaim@gmail.com>
2026-04-18 20:58:17 +0300

    commit1

---
+++ operator presidence.py
@@ -0,0 +1,21 @@
+q = 5
+w = 10
+r = 90
+x = 12
+t = 2
+y = 1
+z = 0
+
+z = (q + r) / (t + y)
+
+print("The value of (q+r) diveded by (t+y) is equal to", z)
+
+name = "alex" 
+age = 0
+
+if name == "alex" or name == "john" and age >=2:
+    print("Hello ! welcome")
+
+else:
+    print("Goodbyee!")
+
